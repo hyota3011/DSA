6215a2de15c412044bd8d34c89d49e305f500a20
Hoang Trung Vu <165503227+hyota3011@users.noreply.github.com>
2024-06-27 22:25:57 +0700

    update nhe

--- Graph Theory/kruskal.cpp
+++ Graph Theory/kruskal.cpp
@@ -7,98 +7,104 @@ using namespace std;
 typedef long long ll;
 
 const int N = 1e2;
-int n,m,lab[N];
+int n,m,trace[N],par[N];
 bool connected = false;
 struct Edge{
     int u,v,w;
     bool mark;
-    Edge(int u=0,int v=0,int w=0):u(u),v(v),w(w){mark=false;}
-} xintv[N];
-void swap(Edge *a,Edge *b){
-    Edge tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-vector<pair<int,int>> g[N];
-int get_root(int v){
-    while(lab[v]>0) v=lab[v];
-    return v;
-}
-void union_ducoh(int r1,int r2){
-    int x = lab[r1] + lab[r2];
-    if(lab[r1] > lab[r2]){
-        lab[r1] = r2;
-        lab[r2] = x;
-    }else{
-        lab[r2] = r1;
-        lab[r1] = x;
+    Edge(int _u=0,int _v=0,int _w=0):u(_u),v(_v),w(_w){mark=false;}
+    static void swap(Edge &a,Edge &b){
+        Edge tmp = a;
+        a=b;
+        b=tmp;
     }
-}
-void heapify(Edge arr[], int n, int i){
-    int idx = i, l = 2*i, r = 2*i+1;
-    if (l <= n && arr[l].w < arr[idx].w) idx = l;
-    if (r <= n && arr[r].w < arr[idx].w) idx = r;
-    if (idx != i){
-        swap(arr[i], arr[idx]);
-        heapify(arr, n, idx);
-    }
-}
+}g[N];
 
-void kruskal(){
-    int cnt = 0;
-    //create heap
-    for(int i=m>>1;i>=1;--i){
-        heapify(xintv,m,i);
-    }
-    for(int i=m;i>=2;--i){
-        swap(xintv[1],xintv[i]);
-        heapify(xintv,i-1,1);
-        
-        int r1 = get_root(xintv[i].u);
-        int r2 = get_root(xintv[i].v);
-        if(r1!=r2){
-            xintv[i].mark=true;
-            ++cnt;
-            if(cnt==n-1){
-                connected = true;
-                return void();
-            }
-            union_ducoh(r1,r2);
+class Heap{
+public:
+    static void heapify(Edge arr[], int i, int n){
+        int idx = i,l=i<<1,r=i<<1|1;
+        if(l<=n&&arr[l].w<arr[idx].w)idx=l;
+        if(r<=n&&arr[r].w<arr[idx].w)idx=r;
+        if(idx!=i){
+            Edge::swap(arr[idx],arr[i]);
+            heapify(arr,idx,n);
         }
     }
-}
-void init_view_detail(){
-    if(connected){
-        cout << "Minimal spanning tree: \n";
+};
+class Kruskal{
+public:
+    int get_root(int u){
+        while(par[u]>0) u = par[u];
+        return u;
+    }
+    void union_tree(int u,int v){
+        u = get_root(u);
+        v = get_root(v);
+        if(u!=v){
+            int sum = par[u] + par[v];
+            if(par[u] > par[v]){
+                par[u] = v;
+                par[v] = sum;
+            }else{
+                par[v] = u;
+                par[u] = sum;
+            }
+        }
+    }
+    void main(Edge arr[],int m,int n){
         int cnt = 0;
-        int w = 0;
-        for(int i=1;i<=m;++i){
-            if(xintv[i].mark){
-                printf("(%d,%d) = %d\n",xintv[i].u,xintv[i].v,xintv[i].w);
+        for(int i=m>>1;i>=1;--i) Heap::heapify(g,i,m);
+        for(int i=m;i>1;--i){
+            Edge::swap(arr[1],arr[i]);
+            Heap::heapify(g,1,i-1);
+            
+            Edge &last = arr[i];
+            int r1 = get_root(last.u), r2 = get_root(last.v);
+            if(r1!=r2){
+                last.mark=true;
                 ++cnt;
-                w+=xintv[i].w;
+                if(cnt == n-1){
+                    connected = true;
+                    return void();
+                }
+                union_tree(r1, r2);
             }
-            if(cnt == n-1) break;
+            
         }
-        cout << "Weight = " << w << endl;
-    }else{
-        cout << "Not connected\n";
     }
-}
+    void print_result(){
+        if(connected){
+            cout << "Minimal spanning tree: \n";
+            int cnt = 0;
+            int w = 0;
+            for(int i=1;i<=m;++i){
+                if(g[i].mark){
+                    printf("(%d,%d) = %d\n",g[i].u,g[i].v,g[i].w);
+                    ++cnt;
+                    w+=g[i].w;
+                }
+                if(cnt == n-1) break;
+            }
+            cout << "Weight = " << w << endl;
+        }else{
+            cout << "Not connected\n";
+        }
+    }
+};
+
 int main(){
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
-    
     cin>>n>>m;
     for(int i=1,u,v,w;i<=m;++i){
         cin>>u>>v>>w;
-        xintv[i] = {u,v,w};
-        g[u].push_back({v,w});
+        g[i] = {u,v,w};
     }
-    memset(lab,-1,sizeof(lab));
-    kruskal();
-    init_view_detail();
-    
+    memset(par,-1,sizeof(par));
+    Kruskal ks;
+    ks.main(g, m, n);
+    ks.print_result();
     /*
  6 9
  1 2 1
